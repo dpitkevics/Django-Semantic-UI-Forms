c4629db87156b3e46d462517903e62734661c0a6
dpitkevics <daniels.pitkevics@gmail.com>
2015-09-02 07:15:29 +0000

    Added manifest file

---
+++ setup.py
@@ -0,0 +1,15 @@
+from distutils.core import setup
+
+
+setup(
+    name = 'semanticui_forms',
+    packages = ['semanticui_forms'],
+    version = '0.1',
+    description = 'Django forms extension for Semantic UI.',
+    author = 'Daniels Pitkevics',
+    author_email = 'daniels.pitkevics@gmail.com',
+    url = 'https://github.com/dpitkevics/Django-Semantic-UI-Forms',
+    download_url = 'https://github.com/dpitkevics/Django-Semantic-UI-Forms/tarball/0.1',
+    keywords = ['django', 'forms', 'Semantic UI'],
+    classifiers = [],
+)
