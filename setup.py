9c78b1fee3ed8c736dd45861a675c1c78f86983e
dpitkevics <daniels.pitkevics@gmail.com>
2015-09-02 12:47:44 +0000

    Tag fix

--- setup.py
+++ setup.py
@@ -4,12 +4,12 @@ from distutils.core import setup
 setup(
     name = 'semanticui_forms',
     packages = ['semanticui_forms'],
-    version = '0.1.3',
+    version = '0.1.4',
     description = 'Django forms extension for Semantic UI.',
     author = 'Daniels Pitkevics',
     author_email = 'daniels.pitkevics@gmail.com',
     url = 'https://github.com/dpitkevics/Django-Semantic-UI-Forms',
-    download_url = 'https://github.com/dpitkevics/Django-Semantic-UI-Forms/tarball/0.1.1',
+    download_url = 'https://github.com/dpitkevics/Django-Semantic-UI-Forms/tarball/0.1.4',
     keywords = ['django', 'forms', 'Semantic UI'],
     classifiers = [],
 )
